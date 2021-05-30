23111df853e9ce3d8023bbc98fd2b56c75f5f9fe
ZOE <theonlyoneu@daum.net>
2021-05-30 23:14:42 +0900

    Commit changes

---
+++ src/components/views/dashboard/index.js
@@ -0,0 +1,63 @@
+import { Result } from 'antd';
+import React, { useEffect } from 'react';
+import LayoutPage from '../layout/LayoutPage';
+import { SmileOutlined } from '@ant-design/icons';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { getUserAll } from '../../../store/user';
+import { withRouter } from 'react-router';
+import { getStatus } from '../../../store/journey';
+import { getProjectAll, getProjectOne } from '../../../store/project';
+import { getEquipAll } from '../../../store/equipment';
+
+const DashboardPage = () => {
+  const user = useSelector(
+    ({ user }) => ({
+      isAuth: user.isAuth,
+      isAdmin: user.isAdmin,
+      loginSuccess: user.loginSuccess,
+      userData: user.userData
+    }),
+    shallowEqual
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!user.loginSuccess) return;
+
+    if (user.isAuth) {
+      dispatch(getStatus());
+      dispatch(getEquipAll());
+
+      if (user.isAdmin) {
+        dispatch(getUserAll());
+        dispatch(getProjectAll()).then((res) => {
+          const projectIds = res.payload.map((item) => item.id);
+          for (let i = 0; i < projectIds.length; i += 1) {
+            dispatch(getProjectOne(projectIds[i]));
+          }
+        });
+      } else {
+        const projects = user.userData.projects;
+        const projectIds = projects.map((item) => item.id);
+
+        for (let i = 0; i < projectIds.length; i += 1) {
+          dispatch(getProjectOne(projectIds[i]));
+        }
+      }
+    }
+  }, [user.isAdmin, user.isAuth]);
+
+  return (
+    <LayoutPage>
+      <div className="inner-container">
+        <Result
+          icon={<SmileOutlined />}
+          title="Welcome to Dashboard!"
+          style={{ padding: '25vh 0' }}
+        />
+      </div>
+    </LayoutPage>
+  );
+};
+
+export default withRouter(DashboardPage);
